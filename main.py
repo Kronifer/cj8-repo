765ef8d4a74bdf20307cdde1db2244980766c5cc
Umer Shaikh <umer.f.shaikh@gmail.com>
2021-07-11 16:59:53 -0500

    Add a custom assert function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import typing as t
+
 import blessed as b
 
 term = b.Terminal()
 TERM_HEIGHT = term.height
 TERM_WIDTH = term.width
 term = term
+
+
+def assert_(testCond: t.bool) -> None:
+    """Custom asset function."""
+    if not testCond:
+        raise AssertionError
