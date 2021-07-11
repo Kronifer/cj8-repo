f6deab0c92f185ccbd1f6330171fdf5dd524b886
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-07-10 21:42:02 -0700

    Add main game loop and other startup code
    Cherry picked from cacdfbec.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,81 @@
-import typing as t
+# Name: main.py
+# Purpose: Runs the main engine of the game
+# Authors: @giode#1018 and @Kronifer#5647
+# Date: July 2021
+#
+
+
+import time
 
 import blessed as b
 
-term = b.Terminal()
-TERM_HEIGHT = term.height
-TERM_WIDTH = term.width
-term = term
+# constants
+FRAMES_PER_SECOND = 60
+SECONDS_PER_FRAME = FRAMES_PER_SECOND ** -1
+
+# globals
+term: b.Terminal
+term_height: int
+term_width: int
 
 
-def assert_(testCond: t.bool) -> None:
+# utility functions
+def assert_(testCond: bool) -> None:
     """Custom asset function."""
     if not testCond:
         raise AssertionError
+
+
+def main() -> None:
+    """The driver function of the Game
+
+    :return: None
+    """
+    # Setup
+    global term
+    global term_height
+    global term_width
+
+    term = b.Terminal()
+    term_height = term.height
+    term_width = term.width
+
+    if term.height < 24 or term.width < 80:
+        print("Terminal must be at least 80x24, aborting.")
+        quit()
+
+    term.clear()
+
+    # Main Game loop conditionals
+    game_over = False
+    start_time, end_time = 0.0, 0.0
+    counter = 1200
+
+    # Main Game Loop
+    with term.fullscreen(), term.cbreak(), term.hidden_cursor():
+        while not game_over:
+
+            # Getting start time of execution
+            start_time = time.time()
+
+            # TODO: Update Screen
+
+            # TODO: Draw to the screen
+
+            # Check to see if the current frame has been shown for enough time to maintain
+            # the current amount of frames per second
+            end_time = time.time()
+            if (time_passed := end_time - start_time) < SECONDS_PER_FRAME - 0.0001:
+
+                time.sleep(SECONDS_PER_FRAME - time_passed)
+
+            # Loop until counter is 0
+            counter -= 1
+
+            if counter < 1:
+                game_over = True
+
+
+# Run contents if file is the driver file
+if __name__ == "__main__":
+    main()
