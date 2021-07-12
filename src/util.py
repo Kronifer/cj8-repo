ad8d2731a56fcb250a3e1144817c2332eb5e3d72
12944qwerty <qwerty12944qwerty@gmail.com>
2021-07-12 18:14:33 -0500

    array to display converter

--- src/util.py
+++ src/util.py
@@ -1,7 +1,23 @@
 """Utility functions."""
 
+import typing as t
+
+KEY = {
+    '': ' ',
+    'AIR': ' ',
+    'WATER': '[blue]█[/blue]',
+    'GRASS': '[green]█[/green]',
+    'LAVA': '[red]█[/red]',
+    'ROCK': '[grey39]█[/grey39]',
+}
+
 
 def assert_(testCond: bool) -> None:
     """Custom asset function."""
     if not testCond:
         raise AssertionError
+
+
+def convert_data(data: t.List[t.List[str]]) -> t.List[t.List[str]]:
+    """Converts level creator data."""
+    return [[KEY[col.upper()] for col in row] for row in data]
