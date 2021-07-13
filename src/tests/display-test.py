8f4c025f925637c8f7bea07ae3f30bb9cf7b8f13
Umer Shaikh <umer.f.shaikh@gmail.com>
2021-07-12 22:23:34 -0500

    Add tests for making and displaying menus
    Also refactors tests in display-test.py so repetition of display setup
    code isn't needed.

--- src/tests/display-test.py
+++ src/tests/display-test.py
@@ -7,6 +7,7 @@ for inspection.
 
 import pickle
 import time
+import typing as t
 import unittest
 
 import env
@@ -19,40 +20,36 @@ import window
 class TestDisplay(unittest.TestCase):
     """Test the display."""
 
-    def test_display(self) -> None:
-        """Main function to test the display."""
+    def run(self, result: t.Optional[unittest.TestResult] = None) -> t.Optional[unittest.TestResult]:
+        """Override built-in run so tests are run in an appropriate context."""
         if env.term.height < 24 or env.term.width < 80:
             print("Terminal must be at least 80x24, aborting.")
             quit()
         env.term.clear()
         with env.term.fullscreen(), env.term.cbreak(), env.term.hidden_cursor():
+            return super(TestDisplay, self).run(result)
 
-            root_data = [["[red]0[/red]"] * 80 for _ in range(24)]
-            child_data = [["[blue]1[/blue]"] * 11 for _ in range(5)]
+    def test_display(self) -> None:
+        """Test basic Panel and Window features."""
+        root_data = [["[red]0[/red]"] * 80 for _ in range(24)]
+        child_data = [["[blue]1[/blue]"] * 11 for _ in range(5)]
 
-            root_panel: ui.Panel = ui.Panel(space.Point(0, 0), space.Point(23, 79), root_data)
-            child_panel: ui.Panel = ui.Panel(space.Point(10, 5), space.Point(14, 15),
-                                             child_data, border_style="green", border=True)
+        root_panel: ui.Panel = ui.Panel(space.Point(0, 0), space.Point(23, 79), root_data)
+        child_panel: ui.Panel = ui.Panel(space.Point(10, 5), space.Point(14, 15),
+                                         child_data, border_style="green", border=True)
 
-            window.panel_stack = [root_panel, child_panel]
-            window.render()
-            time.sleep(2)
-            window.render()
-            time.sleep(2)
-            root_data[0][0] = "[blue]0[/blue]"
-            window.render()
-            time.sleep(2)
-            root_data[0] = ["[blue]0[/blue]"] * env.term.width
-            window.render()
-            time.sleep(2)
+        window.panel_stack = [root_panel, child_panel]
+        window.render()
+        time.sleep(2)
+        root_data[0][0] = "[blue]0[/blue]"
+        window.render()
+        time.sleep(2)
+        root_data[0] = ["[blue]0[/blue]"] * env.term.width
+        window.render()
+        time.sleep(2)
 
     def test_level_display(self) -> None:
         """Test for displaying level saves."""
-        if env.term.height < 24 or env.term.width < 80:
-            print("Terminal must be at least 80x24, aborting.")
-            quit()
-        env.term.clear()
-
         with open('tests/save.level', 'rb') as f:
             data = pickle.load(f)  # noqa: S301
 
@@ -73,17 +70,37 @@ class TestDisplay(unittest.TestCase):
             ['ROCK', 'ROCK', 'ROCK', 'ROCK'],
         ]
 
-        with env.term.fullscreen(), env.term.cbreak(), env.term.hidden_cursor():
-            root_data = util.convert_data(data)
-            rock_data = util.convert_data(rock_data)
+        root_data = util.convert_data(data)
+        rock_data = util.convert_data(rock_data)
 
-            root_panel: ui.Panel = ui.Panel(space.Point(0, 0), space.Point(len(root_data) - 1,
-                                            len(root_data[0]) - 1), root_data)
-            rock_panel: ui.Panel = ui.Panel(space.Point(0, len(root_data[0]) + 4),
-                                            space.Point(len(rock_data) - 1,
-                                            len(root_data[0]) + 4 + len(rock_data[0]) - 1),
-                                            rock_data)
+        root_panel: ui.Panel = ui.Panel(space.Point(0, 0), space.Point(len(root_data) - 1,
+                                        len(root_data[0]) - 1), root_data)
+        rock_panel: ui.Panel = ui.Panel(space.Point(0, len(root_data[0]) + 4),
+                                        space.Point(len(rock_data) - 1,
+                                        len(root_data[0]) + 4 + len(rock_data[0]) - 1),
+                                        rock_data)
 
-            window.panel_stack = [root_panel, rock_panel]
-            window.render()
-            time.sleep(2)
+        window.panel_stack = [root_panel, rock_panel]
+        window.render()
+        time.sleep(2)
+
+    def test_menus(self) -> None:
+        """Test menus."""
+        menu: ui.Menu = ui.Menu([ui.MenuEntry("foo"),
+                                 ui.MenuEntry("bar"),
+                                 ui.MenuEntry("baz"),
+                                 ui.MenuEntry("quuuuuuuux")],
+                                center_entries=True)
+        menu_panel = menu.make_panel()
+        window.panel_stack = [menu_panel]
+        window.render()
+        time.sleep(2)
+        root_data = [["[red]0[/red]"] * 80 for _ in range(24)]
+        root_panel: ui.Panel = ui.Panel(space.Point(0, 0), space.Point(len(root_data) - 1,
+                                        len(root_data[0]) - 1), root_data)
+        menu.maximize = False
+        menu.center_entries = False
+        menu_panel = menu.make_panel()
+        window.panel_stack = [root_panel, menu_panel]
+        window.render()
+        time.sleep(2)
