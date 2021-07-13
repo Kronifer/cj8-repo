c8aa2f84cfdae357090d73d119be02f6b826f9d5
Umer Shaikh <umer.f.shaikh@gmail.com>
2021-07-13 03:38:24 -0500

    Add input handling, refine game loop, scaffolding
    Also adds some non-implemented functions and gets a (not yet functional)
    main menu on screen on startup.

--- src/main.py
+++ src/main.py
@@ -6,12 +6,26 @@
 
 
 import time
+import typing as t
+
+import blessed as b
 
 import env
+import window
 
 # constants
 FRAMES_PER_SECOND = 60
 SECONDS_PER_FRAME = FRAMES_PER_SECOND ** -1
+
+key_to_action: dict[str, t.Callable] = {"KEY_LEFT": env.you.move_left,
+                                        "KEY_RIGHT": env.you.move_right,
+                                        "KEY_SPACE": env.you.jump,
+                                        "KEY_ESCAPE": window.show_main_menu}
+
+
+def update_world() -> None:
+    """Point of entry for updating the simulation."""
+    pass
 
 
 def main() -> None:
@@ -28,7 +42,6 @@ def main() -> None:
     # Main Game loop conditionals
     game_over = False
     start_time, end_time = 0.0, 0.0
-    counter = 1200
 
     # Main Game Loop
     with env.term.fullscreen(), env.term.cbreak(), env.term.hidden_cursor():
@@ -37,22 +50,28 @@ def main() -> None:
             # Getting start time of execution
             start_time = time.time()
 
-            # TODO: Update Screen
+            # Render the world/the main menu/whatever
+            window.display()
 
-            # TODO: Draw to the screen
+            # Get and process input
+            inp: b.keyboard.Keystroke = env.term.inkey(timeout=0)  # timeout=0 doesn't block
+            inp_s = str(inp)
+
+            if env.paused:  # the "simulation" should not tick
+                window.process_input(inp_s)
+
+            # Player acts and world updates
+            else:
+                if inp_s in key_to_action:
+                    key_to_action[str(inp_s)]()
+                if not env.paused:
+                    update_world()
 
             # Check to see if the current frame has been shown for enough time to maintain
             # the current amount of frames per second
             end_time = time.time()
             if (time_passed := end_time - start_time) < SECONDS_PER_FRAME - 0.0001:
-
                 time.sleep(SECONDS_PER_FRAME - time_passed)
-
-            # Loop until counter is 0
-            counter -= 1
-
-            if counter < 1:
-                game_over = True
 
 
 # Run contents if file is the driver file
