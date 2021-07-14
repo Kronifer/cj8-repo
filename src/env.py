d1c9fa9f1a4bcb0e3e3cc89316cc822383cf17de
Umer Shaikh <umer.f.shaikh@gmail.com>
2021-07-14 00:00:41 -0500

    Fix (hopefully) a bug with the screen jittering around
    It seems like some terminal emulators (xterm, whatever windows uses by
    default now) don't like our trying to write to the whole screen, and as
    a result the whole screen content jitters around as if we were trying to
    write to more rows than we have. A somewhat hacky fix is to simply act
    like we have slightly fewer lines than we do.

--- src/env.py
+++ src/env.py
@@ -8,7 +8,9 @@ import blessed
 import player
 
 term: blessed.Terminal = blessed.Terminal()
-term_height: int = term.height
+# some terminal emulators apparently don't deal well with our trying to use the
+# full height, and bounce the screen around
+term_height: int = term.height - 2
 term_width: int = term.width
 
 paused: bool = False
