18b34c2030b86ffe2ef878ed876cbfcc8e01fae7
Kronifer <44979306+Kronifer@users.noreply.github.com>
2021-07-15 11:48:08 -0500

    Moved logo to top

--- src/display.py
+++ src/display.py
@@ -91,9 +91,10 @@ def display(world: list) -> None or list:
     if env.paused:  # Menu displayer
         main_menu_window = main_menu.make_window()
         logo = [list(line) for line in env.logo.split("\n")[1:-1]]
-        logo_origin = space.Point(25, int(main_menu_window.origin.x-3*len(logo[0])/10))  # Centers the logo.
+        logo[-1].append("\n")
+        logo_origin = space.Point(-32, int(main_menu_window.origin.x-3*len(logo[0])/10))  # Centers the logo.
         logo_window = window.Window.from_origin(logo_origin, logo)
-        win_stack = [main_menu_window, logo_window]
+        win_stack = [logo_window, main_menu_window]
     else:  # Game displayer
         root_window = window.Window.from_origin(space.Point(0, 0), util.convert_data(world))
         win_stack = [root_window]
