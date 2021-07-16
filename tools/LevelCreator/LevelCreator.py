740fff313c7bfd273a7c52f30446901ad54cf8ea
Jetsie <badjetsie@gmail.com>
2021-07-16 01:05:36 -1000

    Added an easier way to add tile types & click and drag support.

--- tools/LevelCreator/LevelCreator.py
+++ tools/LevelCreator/LevelCreator.py
@@ -1,59 +1,10 @@
 import pickle
-import tkinter as tk
 
+import ezTypes as ez
 import pygame
 
-LevelWidth, LevelHeight = 0, 0
-
-# Init pygame modules
-pygame.init()
-
-# Get target size of the level
-root = tk.Tk()
-canvas1 = tk.Canvas(root, width=200, height=150)
-canvas1.pack()
-entry1 = tk.Entry(root, width=2)
-entry2 = tk.Entry(root, width=2)
-label1 = tk.Label(root, text='size =')
-label2 = tk.Label(root, text='x')
-
-canvas1.create_window(60, 45, window=label1)
-canvas1.create_window(85, 45, window=entry1)
-canvas1.create_window(100, 45, window=label2)
-canvas1.create_window(115, 45, window=entry2)
-
-
-def setsize() -> None:
-    """Sets the size of the window."""
-    global LevelWidth, LevelHeight
-    LevelWidth = int(entry1.get())
-    LevelHeight = int(entry2.get())
-    root.destroy()
-
-
-button1 = tk.Button(text='Create level', command=setsize)
-canvas1.create_window(100, 80, window=button1)
-
-root.mainloop()
-
-# Declare tile types
-AIR = (0, 0, 0)
-GRASS = (100, 200, 40)
-LAVA = (252, 144, 3)
-WATER = (0, 0, 255)
-PLAYER = (100, 100, 100)
-PLAYER_END = (40, 30, 100)
-SPIKE_UP = (156, 156, 156)
-SPIKE_DOWN = (51, 51, 51)
-ENEMY1 = (200, 0, 0)
-MOV_PLAT = (148, 0, 211)
-
-types = ['GRASS', 'LAVA', 'WATER', 'PLAYER', 'SPIKE_UP', 'SPIKE_DOWN', 'PLAYER_END', 'ENEMY1', 'MOV_PLAT']
-colorTypes = [GRASS, LAVA, WATER, PLAYER, SPIKE_UP, SPIKE_DOWN, PLAYER_END, ENEMY1, MOV_PLAT]
-
-# Set default type
-select = 'GRASS'
-colorSelect = GRASS
+LevelWidth = int(input('Please enter the width of the level: '))
+LevelHeight = int(input('Please enter the height of the level: '))
 
 # Create the window
 screen = pygame.display.set_mode([LevelWidth * 20, (LevelHeight * 20) + 20])
@@ -83,14 +34,15 @@ while True:
             # Place or remove tiles on the map
             if 0 < event.pos[0] < LevelWidth * 20 and 20 < event.pos[1] < ((LevelHeight + 1) * 20):
                 if event.button in [1, 3]:
-                    grid[(event.pos[1] // 20)-1][event.pos[0] // 20] = select if event.button == 1 else ''
-                    colorGrid[(event.pos[1] // 20)-1][event.pos[0] // 20] = colorSelect if event.button == 1 else AIR
+                    grid[(event.pos[1] // 20) - 1][event.pos[0] // 20] = ez.select if event.button == 1 else ''
+                    colorGrid[(event.pos[1] // 20) - 1][
+                        event.pos[0] // 20] = ez.colorSelect if event.button == 1 else ez.AIR
             # Tile selection menu
             if 0 < event.pos[0] < LevelWidth * 20 and 0 < event.pos[1] < 20 and event.button == 1:
                 xUnit = event.pos[0] // 20
                 try:
-                    select = types[xUnit]
-                    colorSelect = colorTypes[xUnit]
+                    ez.select = ez.types[xUnit]
+                    ez.colorSelect = ez.colorTypes[xUnit]
                 except IndexError:
                     pass
             # Export button
@@ -100,14 +52,20 @@ while True:
         if event.type == pygame.MOUSEMOTION:
             if 0 < event.pos[0] < LevelWidth * 20 and 0 < event.pos[1] < 20:
                 try:
-                    pygame.display.set_caption(f'Select tile type: {types[event.pos[0] // 20]}')
+                    pygame.display.set_caption(f'Select tile type: {ez.types[event.pos[0] // 20]}')
                 except IndexError:
-                    if event.pos[0]//20 == LevelWidth-1:
+                    if event.pos[0] // 20 == LevelWidth - 1:
                         pygame.display.set_caption('Export')
                     else:
                         pygame.display.set_caption('Level Creator')
             else:
                 pygame.display.set_caption('Level Creator')
+                if event.buttons[0] == 1 or event.buttons[2] == 1:
+                    grid[(event.pos[1] // 20) - 1][event.pos[0] // 20] = ez.select if event.buttons[0] == 1 else ''
+                    if event.buttons[0] == 1:
+                        colorGrid[(event.pos[1] // 20) - 1][event.pos[0] // 20] = ez.colorSelect
+                    else:
+                        colorGrid[(event.pos[1] // 20) - 1][event.pos[0] // 20] = ez.AIR
 
     # Draw the map tiles
     for y in range(len(grid)):
@@ -116,8 +74,8 @@ while True:
                 drawsq(colorGrid[y][x], (x * 20, (y + 1) * 20))
 
     # Draw the tile selection bar
-    for i in range(len(types)):
-        drawsq(colorTypes[i], (i * 20, 0))
+    for i in range(len(ez.types)):
+        drawsq(ez.colorTypes[i], (i * 20, 0))
 
     # Draw the yellow export button
     drawsq((252, 231, 3), ((LevelWidth - 1) * 20, 0))
---
+++ tools/LevelCreator/ezTypes.py
@@ -0,0 +1,19 @@
+# List the type colors for the editor
+AIR = (0, 0, 0)
+GRASS = (100, 200, 40)
+LAVA = (252, 144, 3)
+WATER = (0, 0, 255)
+PLAYER = (100, 100, 100)
+PLAYER_END = (40, 30, 100)
+SPIKE_UP = (156, 156, 156)
+SPIKE_DOWN = (51, 51, 51)
+ENEMY1 = (200, 0, 0)
+MOV_PLAT = (148, 0, 211)
+
+# List all the used types
+types = ['GRASS', 'LAVA', 'WATER', 'PLAYER', 'SPIKE_UP', 'SPIKE_DOWN', 'PLAYER_END', 'ENEMY1', 'MOV_PLAT']
+colorTypes = [GRASS, LAVA, WATER, PLAYER, SPIKE_UP, SPIKE_DOWN, PLAYER_END, ENEMY1, MOV_PLAT]
+
+# Set default type
+select = 'GRASS'
+colorSelect = GRASS
